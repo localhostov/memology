73d60c763b5e7bf08c39a9c195f34225882734bf
Localhostov <localhostov@gmail.com>
2023-11-11 22:43:25 +0300

    fix(tabs): Show desktop or mobile tabs

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,12 +1,31 @@
 import { RouterProvider } from "@vkontakte/vk-mini-apps-router"
-import { AdaptivityProvider, AppRoot, ConfigProvider } from "@vkontakte/vkui"
+import {
+    AdaptivityProvider,
+    AppRoot,
+    ConfigProvider,
+    ViewWidth,
+} from "@vkontakte/vkui"
 import { router } from "../shared"
 import { Epic } from "./Epic"
 
 export const App = () => {
+    const getNamedWindowWidth = () => {
+        const width = window.innerWidth
+
+        if (width > 320 && width < 600) {
+            return ViewWidth.MOBILE
+        }
+        if (width > 600 && width < 1000) {
+            return ViewWidth.TABLET
+        }
+        if (width >= 1000) {
+            return ViewWidth.DESKTOP
+        }
+    }
+
     return (
-        <ConfigProvider hasCustomPanelHeaderAfter={true}>
-            <AdaptivityProvider>
+        <ConfigProvider>
+            <AdaptivityProvider viewWidth={getNamedWindowWidth()}>
                 <AppRoot>
                     <RouterProvider
                         router={router}
