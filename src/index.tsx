61172b3660eb9418af90023696c83e187397996e
Kravets <57632712+kravetsone@users.noreply.github.com>
2023-11-12 14:08:51 +0300

    feat(dev): add effector-redux-devtools

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import "@vkontakte/vkui/dist/vkui.css"
+import { attachReduxDevTools } from "@effector/redux-devtools-adapter"
 import ReactDOM from "react-dom/client"
 import { init } from "./shared"
 import { App } from "./views/App"
+//TODO: only in dev mode
+attachReduxDevTools()
 
 init()
 
