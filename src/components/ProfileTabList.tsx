86f4a4e8bb8183d3d6f0565ea990c3f5deb0f4f5
Localhostov <localhostov@gmail.com>
2023-11-12 21:52:00 +0300

    feat: Add memes loading

--- src/components/ProfileTabList.tsx
+++ src/components/ProfileTabList.tsx
@@ -1,25 +1,34 @@
 import { useRouteNavigator } from "@vkontakte/vk-mini-apps-router"
-import { Search } from "@vkontakte/vkui"
+import { Placeholder, Search, Spinner } from "@vkontakte/vkui"
 import { useList, useUnit } from "effector-react"
 import { useEffect } from "react"
 import { ProfileEffects } from "../shared"
 import styles from "../styles/profile.module.css"
-import { TListType } from "../types"
+import { TProfileTabListType } from "../types"
 import { MemeListItem } from "./MemeListItem"
+import { Icon28SadFaceOutline } from "@vkontakte/icons"
 
 interface Props {
-    type: TListType
+    type: TProfileTabListType
 }
 
-const searchPlaceholder: Record<TListType, string> = {
+const searchPlaceholder: Record<TProfileTabListType, string> = {
     like: "Искать в лайках",
     dislike: "Искать в дизлайках",
     favorite: "Искать в избранных",
 }
 
+const listName: Record<TProfileTabListType, string> = {
+    like: "лайков",
+    dislike: "дизлайков",
+    favorite: "избранных",
+}
+
 export function ProfileTabList({ type }: Props) {
     const navigator = useRouteNavigator()
+    const memes = useUnit(ProfileEffects.$memesList)
     const search = useUnit(ProfileEffects.$memesSearch)
+    const memesIsLoading = useUnit(ProfileEffects.getMemesListFx.pending)
 
     useEffect(() => {
         ProfileEffects.fetchMemes()
@@ -42,7 +51,23 @@ export function ProfileTabList({ type }: Props) {
                 placeholder={searchPlaceholder[type]}
             />
             <div className={styles.tabContentContainer}>
-                <div className={styles.cardsContainer}>{memesList}</div>
+                {memesIsLoading ? (
+                    <Spinner size="medium" />
+                ) : memes.length > 0 ? (
+                    <div className={styles.cardsContainer}>{memesList}</div>
+                ) : (
+                    <Placeholder
+                        icon={
+                            <Icon28SadFaceOutline
+                                style={{ width: 86, height: 86 }}
+                            />
+                        }
+                        header="Ничего не найдено"
+                    >
+                        Похоже, вы еще не добавили такого мема в список{" "}
+                        {listName[type]}
+                    </Placeholder>
+                )}
             </div>
         </>
     )
