979933facc58d889086e8c857b66772e227424b6
Localhostov <localhostov@gmail.com>
2023-11-13 20:17:10 +0300

    feat: Add meme panel with mock data

--- src/panels/Meme.tsx
+++ src/panels/Meme.tsx
@@ -1,11 +1,25 @@
-import { useParams, useRouteNavigator } from "@vkontakte/vk-mini-apps-router"
-import { Group, Panel, PanelHeader, PanelHeaderBack } from "@vkontakte/vkui"
-import { panelNames } from "../shared"
+import { useRouteNavigator } from "@vkontakte/vk-mini-apps-router"
+import {
+    Avatar,
+    Group,
+    Panel,
+    PanelHeader,
+    PanelHeaderBack,
+    SimpleCell,
+} from "@vkontakte/vkui"
+import { MemeItem, panelNames } from "../shared"
 import { IPanelProps } from "../types"
+import styles from "../styles/meme.module.css"
+import bridge from "@vkontakte/vk-bridge"
 
 export const Meme = ({ id }: IPanelProps) => {
     const navigator = useRouteNavigator()
-    const params = useParams<"memeId">()
+
+    const openImage = () => {
+        bridge.send("VKWebAppShowImages", {
+            images: [memeInfo.image],
+        })
+    }
 
     return (
         <Panel id={id}>
@@ -15,7 +29,50 @@ export const Meme = ({ id }: IPanelProps) => {
                 {panelNames[id]}
             </PanelHeader>
 
-            <Group>{params?.memeId}</Group>
+            <Group>
+                <div className={styles.imageContainer}>
+                    <img
+                        src={memeInfo.image}
+                        className={styles.image}
+                        onClick={openImage}
+                        alt=""
+                    />
+
+                    <div>
+                        <div className={styles.title}>{memeInfo.title}</div>
+                        <div className={styles.description}>
+                            {memeInfo.description}
+                        </div>
+
+                        <div style={{ height: 8 }} />
+
+                        <div className={styles.imageContainerInfo}>
+                            <SimpleCell
+                                before={<Avatar size={36} />}
+                                style={{ paddingLeft: 0, borderRadius: 100 }}
+                                subtitle={"Автор мема"}
+                            >
+                                Александр Локалхостов
+                            </SimpleCell>
+
+                            <div className={styles.placeInRating}>
+                                #1 в недельном рейтинге
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Group>
         </Panel>
     )
+}
+
+const memeInfo: MemeItem = {
+    description:
+        "Lorem ipsum dolor dolor dolor dolor dolor dolor dolor dolor dolor dolor dolor dolor dolor dolor dolor dolor dolor dolor",
+    favoritesCount: 0,
+    id: 0,
+    image: "https://www.neredeoku.com/gorsel/ankara/etimesgut/ozel-asfa-ankara-ferda-koleji-098e6f5ecae42c26a940ad4a6c7.jpg",
+    isFavorites: false,
+    likesCount: 0,
+    title: "Some title meme",
 }
