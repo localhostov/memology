a44fad0485e5b5484e881c0fdddb73aa3e33fcbe
Localhostov <localhostov@gmail.com>
2023-11-13 20:15:30 +0300

    feat: Add rating list item UI

--- src/components/RatingListItem.tsx
+++ src/components/RatingListItem.tsx
@@ -1,20 +1,71 @@
 import { RatingItem } from "../panels"
 import styles from "../styles/ratingListItem.component.module.css"
 import { Tappable } from "@vkontakte/vkui"
+import {
+    Icon28ArrowUpRectangleOutline,
+    Icon28BookmarkCircleFillYellow,
+    Icon28BookmarkOutline,
+} from "@vkontakte/icons"
+import { useRouteNavigator } from "@vkontakte/vk-mini-apps-router"
 
-export const RatingListItem = ({ item }: { item: RatingItem }) => {
-    // const openMeme = () => {}
+export const RatingListItem = ({
+    item,
+    place,
+}: {
+    item: RatingItem
+    place: number
+}) => {
+    const navigator = useRouteNavigator()
+    const openMeme = () => {
+        navigator.push(`/meme/${item.id}`)
+    }
 
     return (
-        <Tappable className={styles.card}>
-            <img
-                src={item.image}
-                style={{ background: `url(${item.image})` }}
-                className={styles.memeImage}
-                alt=""
-            />
+        <div key={item.id}>
+            <Tappable className={styles.card} onClick={openMeme}>
+                <div>
+                    <img
+                        src={item.image}
+                        style={{ background: `url(${item.image})` }}
+                        className={styles.memeImage}
+                        alt=""
+                    />
 
-            <div></div>
-        </Tappable>
+                    <div className={styles.place}>{place}</div>
+                </div>
+
+                <div className={styles.memeInfoContainer}>
+                    <div className={styles.memeTitle}>{item.title}</div>
+                    <div className={styles.memeDescription}>
+                        {item.description}
+                    </div>
+
+                    <div style={{ height: 8 }} />
+
+                    <div className={styles.memeInfoCounters}>
+                        <div>
+                            {item.inFavorites ? (
+                                <Icon28BookmarkCircleFillYellow
+                                    style={{ width: 20, height: 20 }}
+                                />
+                            ) : (
+                                <Icon28BookmarkOutline
+                                    style={{ width: 20, height: 20 }}
+                                />
+                            )}
+
+                            {item.favoritesCount}
+                        </div>
+
+                        <div>
+                            <Icon28ArrowUpRectangleOutline
+                                style={{ width: 20, height: 20 }}
+                            />
+                            {item.likesCount}
+                        </div>
+                    </div>
+                </div>
+            </Tappable>
+        </div>
     )
 }
