f66fba3898377646fd426e231dc582328a5c152d
Localhostov <localhostov@gmail.com>
2023-11-15 21:01:34 +0300

    feat: Add content to CreateMemeCommentModal

--- src/modals/CreateMemeCommentModal/CreateMemeCommentModal.tsx
+++ src/modals/CreateMemeCommentModal/CreateMemeCommentModal.tsx
@@ -1,9 +1,46 @@
 import { IModalProps } from "@types"
-import { useParams } from "@vkontakte/vk-mini-apps-router"
-import { ModalCard } from "@vkontakte/vkui"
+import { useParams, useRouteNavigator } from "@vkontakte/vk-mini-apps-router"
+import { Button, FormItem, ModalCard, Textarea } from "@vkontakte/vkui"
+import { useState } from "react"
 
 export const CreateMemeCommentModal = ({ id }: IModalProps) => {
     const params = useParams<"memeId">()
+    const navigator = useRouteNavigator()
+    const [text, setText] = useState("")
 
-    return <ModalCard id={id}>memeId: {params?.memeId}</ModalCard>
+    const createComment = () => {
+        console.log(`user created comment on meme with id ${params?.memeId}`)
+        navigator.hideModal()
+    }
+
+    return (
+        <ModalCard id={id}>
+            <FormItem
+                top={`Опишите увиденное • ${
+                    text.trim().length
+                } / ${MAX_COMMENT_LENGTH}`}
+                style={{ padding: 0 }}
+            >
+                <Textarea
+                    value={text}
+                    maxLength={MAX_COMMENT_LENGTH}
+                    onChange={(e) => setText(e.target.value)}
+                    placeholder="Этот мем такой классный, смеялись всем селом"
+                />
+            </FormItem>
+
+            <div style={{ height: 16 }} />
+
+            <Button
+                size={"l"}
+                mode={text.trim().length > 0 ? "primary" : "secondary"}
+                disabled={text.trim().length === 0}
+                onClick={createComment}
+            >
+                Оставить комментарий
+            </Button>
+        </ModalCard>
+    )
 }
+
+const MAX_COMMENT_LENGTH = 256
