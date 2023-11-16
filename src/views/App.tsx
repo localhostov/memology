bf818809eb5b128b8036dc11f4e1a8335554f108
Localhostov <localhostov@gmail.com>
2023-11-16 14:18:45 +0300

    feat: Add navigation bars colors

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,10 +1,22 @@
-import { getNamedWindowWidth, getUser, getVkUser, router } from "@shared"
+import {
+    bridgeSetupNavColors,
+    getNamedWindowWidth,
+    getUser,
+    getVkUser,
+    router,
+} from "@shared"
 import { RouterProvider } from "@vkontakte/vk-mini-apps-router"
-import { AdaptivityProvider, AppRoot, ConfigProvider } from "@vkontakte/vkui"
+import {
+    AdaptivityProvider,
+    AppRoot,
+    ConfigProvider,
+    useAppearance,
+} from "@vkontakte/vkui"
 import { useCallback, useEffect, useState } from "react"
 import { Epic } from "./Epic"
 
 export const App = () => {
+    const appearance = useAppearance()
     const [windowWidth, setWindowWidth] = useState(
         getNamedWindowWidth(window.innerWidth),
     )
@@ -17,6 +29,21 @@ export const App = () => {
         getUser()
         getVkUser()
     }, [])
+
+    useEffect(() => {
+        const statusBarColor = getComputedStyle(
+            document.documentElement,
+        ).getPropertyValue("--background-content")
+        const navbarColor = getComputedStyle(
+            document.documentElement,
+        ).getPropertyValue("--tabbar-background")
+
+        bridgeSetupNavColors({
+            appearance,
+            statusBarColor,
+            navbarColor,
+        })
+    }, [appearance])
 
     useEffect(() => {
         window.addEventListener("resize", handleWindowResize)
