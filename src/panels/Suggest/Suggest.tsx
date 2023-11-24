f9bc32f65fc04c2e3c0a454c306c30c0be72c0c3
Localhostov <localhostov@gmail.com>
2023-11-24 19:15:28 +0300

    feat(Suggest): Add content

--- src/panels/Suggest/Suggest.tsx
+++ src/panels/Suggest/Suggest.tsx
@@ -1,11 +1,16 @@
 import { panelNames } from "@shared"
 import { IPanelProps } from "@types"
+import { Icon56GalleryOutline } from "@vkontakte/icons"
 import { useRouteNavigator } from "@vkontakte/vk-mini-apps-router"
 import {
+    Button,
+    FormItem,
     Group,
+    Input,
     Panel,
     PanelHeader,
     PanelHeaderBack,
+    Textarea,
     Title,
 } from "@vkontakte/vkui"
 import { useCallback, useState } from "react"
@@ -15,6 +20,9 @@ import styles from "./styles.module.css"
 export const Suggest = ({ id }: IPanelProps) => {
     const navigator = useRouteNavigator()
     const [image, setImage] = useState<string | ArrayBuffer | null>(null)
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+
     const onDrop = useCallback((acceptedFiles: File[]) => {
         acceptedFiles.forEach((file) => {
             const reader = new FileReader()
@@ -46,26 +54,69 @@ export const Suggest = ({ id }: IPanelProps) => {
                 <div className={styles.imageContainer}>
                     <div {...getRootProps()}>
                         <input className="input-zone" {...getInputProps()} />
-                        {image ? (
-                            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                            // @ts-ignore
-                            <img src={image} className={styles.image} alt="" />
-                        ) : (
-                            <div
-                                className={`${styles.pickBlock} ${
-                                    isDragActive ? styles.dragActive : undefined
-                                }`}
-                            >
-                                <Title level="2">Картинка мема</Title>
-                                <div>
-                                    Нажмите или перенесите файл в эту зону,
-                                    чтобы загрузить
-                                </div>
-                            </div>
-                        )}
+
+                        <div
+                            className={`${styles.pickBlock} ${
+                                isDragActive ? styles.dragActive : undefined
+                            }`}
+                            style={
+                                image
+                                    ? {
+                                          background: `url(${image})`,
+                                          backgroundSize: "cover",
+                                          backgroundRepeat: "no-repeat",
+                                          border: "none",
+                                      }
+                                    : undefined
+                            }
+                        >
+                            {!image && <Icon56GalleryOutline />}
+                        </div>
                     </div>
 
-                    <div className={styles.imageContainerContent} />
+                    <div className={styles.imageContainerContent}>
+                        <Title level="2">Картинка мема</Title>
+                        <div
+                            style={{
+                                color: "var(--vkui--color_text_secondary)",
+                            }}
+                        >
+                            Нажмите или перенесите файл в эту зону, чтобы
+                            загрузить
+                        </div>
+
+                        <div style={{ height: 16 }} />
+
+                        <FormItem bottom="Название мема" style={{ padding: 0 }}>
+                            <Input
+                                placeholder="Придумайте или напишите если знаете"
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                            />
+                        </FormItem>
+                    </div>
+                </div>
+
+                <FormItem top="Описание мема" style={{ padding: "0 16px" }}>
+                    <Textarea
+                        placeholder="Напишите, о чём этот мем, о его происхождении"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                </FormItem>
+
+                <div style={{ padding: 16 }}>
+                    <Button
+                        size="l"
+                        disabled={
+                            !image ||
+                            title.trim().length === 0 ||
+                            description.trim().length === 0
+                        }
+                        stretched
+                    >
+                        Отправить на рассмотрение
+                    </Button>
                 </div>
             </Group>
         </Panel>
