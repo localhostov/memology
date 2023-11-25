6f524bcf156081faa8b786bd20b032a9039ad6f8
Kravets <57632712+kravetsone@users.noreply.github.com>
2023-11-25 18:00:20 +0300

    fix(effects): fix resets

--- src/shared/effects/calls.ts
+++ src/shared/effects/calls.ts
@@ -1,10 +1,8 @@
-import { disconnectWs, GamesEffects } from "@shared"
 import bridge from "@vkontakte/vk-bridge"
 import { createEffect, createEvent, restore, sample } from "effector"
 
 export const setCallLink = createEvent<string | null>()
 export const $callLink = restore(setCallLink, null)
-$callLink.reset(GamesEffects.History.$isStarted, disconnectWs)
 
 export const joinToCallFx = createEffect((link: string) =>
     bridge.send("VKWebAppCallJoin", { join_link: link }),
